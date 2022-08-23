30d843e50633fdc2a21d3f6bb8b5f35f68eecd21
Nilesh Vishwakarma <nileshv@axioned.com>
2022-08-23 18:34:43 +0530

    digital clock functionaliy done in script file

--- assets/js/script.js
+++ assets/js/script.js
@@ -19,3 +19,35 @@ function changeSrc() {
     image.src = srcArr[counter];
   }
 }
+
+// for digital clock
+var hour = document.querySelector('.hour-number'),
+  minute = document.querySelector('.minute-number'),
+  second = document.querySelector('.second-number');
+
+setInterval(clock, 1000);
+
+function clock() {
+  var date = new Date(),
+    h = date.getHours(),
+    m = date.getMinutes(),
+    s = date.getSeconds(),
+    afterTwelve = h - 12;
+
+  if (h > 12) {
+    zero(afterTwelve, hour);
+  } else {
+    hour.innerText = h;
+  }
+
+  zero(m, minute);
+  zero(s, second);
+}
+
+function zero(time, element) {
+  if (time < 10) {
+    element.innerText = '0' + time;
+  } else {
+    element.innerText = time;
+  }
+}
